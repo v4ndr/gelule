9f715d2dab7fa4cc611e0b1755154a754e63d89d
v4ndr <romain.vdp@gmail.com>
2022-11-23 23:32:12 +0100

    iframe style isolation

--- client/content.js
+++ client/content.js
@@ -2,15 +2,7 @@ if (typeof init === 'undefined') {
     const inject = () => { 
         var modal = document.createElement('iframe');
         modal.src =  chrome.runtime.getURL('sample.html');
-        modal.style.width = '100%';
-        modal.style.position = 'fixed';
-        modal.style.top = '0';
-        modal.style.left = '0';
-        modal.style.margin = 0;
-        modal.style.padding = 0;
-        modal.style.zIndex = 10000;
-        modal.style.backgroundColor = 'white';
-        modal.style.border = 'none';
+        modal.id = 'gelcr23';
         document.body.appendChild(modal);
     }
     inject();
